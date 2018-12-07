6188bec2a3d532f494b6fb672dee109444b238ef
wurunwu <2818511381@qq.com>
2018-12-07 18:06:41 +0800

    舒服舒服1221

--- .idea/main/java/wu/RedisApp.java
+++ .idea/main/java/wu/RedisApp.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
 public class RedisApp {
     public static void main(String[] args) {
+        System.out.println("aa");
         SpringApplication.run(RedisApp.class,args);
     }
 }
